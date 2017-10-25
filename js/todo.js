def1b634b353b7c0f3540a80216c71e4bcf2f68e
angel917k <angel917k@gmail.com>
2017-10-25 13:46:16 -0700

    Todo app v1

---
+++ js/todo.js
@@ -0,0 +1,71 @@
+function addListItem (){
+
+  var ul = document.getElementById('ul'); //grab ul to use for appending userinputs
+
+  var todoItem = document.createElement('li'); //create li
+    todoItem.classList.add('todo-item');
+    todoItem.classList.add('center-block');
+
+  var userInput = document.getElementById('todo').value; //value for user input
+  var pTag = document.createElement('p'); //Creates p tag to hold user input
+  pTag.innerHTML = '&nbsp;' + userInput; //places user input inside of p tag
+
+  var completeButton = document.createElement('div'); // creates complete button div
+  completeButton.classList.add('completed-button'); // add class to complete button div
+  completeButton.addEventListener('click',function(e){
+    currentClass=this.classList;
+
+    if(currentClass.value==="completed-button"){
+      var parent = this.parentNode;
+      this.classList.remove('completed-button');
+      this.classList.add('completed-button-done');
+      parent.classList.remove('todo-item');
+      parent.classList.add('todo-item-completed');
+      parent.removeChild(parent.childNodes[1]);
+    } else {
+      this.classList.remove('completed-button-done');
+      this.classList.add('completed-button');
+      var parent = this.parentNode;
+      parent.classList.remove('todo-item-completed');
+      parent.classList.add('todo-item');
+      var reappendEdit = document.createElement('div');
+      reappendEdit.classList.add('edit-button');
+      parent.insertBefore(reappendEdit,parent.childNodes[1]);
+    }
+
+  }); // crosses out user input and removes edit button when clickes. removes crossout and adds edit button
+
+  var editButton = document.createElement('div'); // creates edit button div
+  editButton.classList.add('edit-button'); // adds edit button class to div
+  editButton.addEventListener('click',function(e){
+    var userEdit = prompt('What would you like to do instead?');
+    var parent = this.parentNode;
+    var thisPTag = parent.childNodes[3];
+    thisPTag.innerHTML = '&nbsp;' + userEdit
+  }); // Edits current todo item
+
+  var deleteButton = document.createElement('div'); //adds delete button div
+  deleteButton.classList.add('delete-button'); // adds delete button class to div
+  deleteButton.addEventListener('click', function(e){
+    var confirmDelete = confirm("Wow there sailor, are you sure you want delete this todo item?");
+    if(confirmDelete==true){
+    var currentItem = this.parentNode;
+    var currentItemParent = currentItem.parentNode;
+    currentItemParent.removeChild(currentItem);
+  }
+  else{
+  }
+}); // deletes the current node by prompting user.
+
+  todoItem.appendChild(completeButton);//  adds complete button to li
+  todoItem.appendChild(editButton); // adds edit button to li
+  todoItem.appendChild(deleteButton);//  adds delete button to li
+  todoItem.appendChild(pTag); // adds user input to li
+  ul.appendChild(todoItem); // adds entire li to ul
+
+  document.getElementById('todo').value = ''; //Resets userinput field
+
+}
+
+var add = document.getElementById('add-button') // obtains add pencil button
+add.addEventListener('click', addListItem) // triggers functionality 
